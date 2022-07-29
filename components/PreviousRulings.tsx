b75c47712ad1bacbeb6afaec5f69f3a4fea828da
hectorfhurtado <hurtado.fernando@gmail.com>
2022-07-29 17:19:14 -0500

    add previous rulings initial component

---
+++ components/PreviousRulings.tsx
@@ -0,0 +1,20 @@
+
+export default function PreviousRulings ()
+{
+    return (
+        <div>
+            <div className="flex justify-between items-center">
+                <h3 className="text-gray-600 text-[28px] md:text-[24px] md:leading-[28.8px] xl:text-[45px] font-light leading-7">Previous Rulings</h3>
+
+            </div>
+
+            <div className="cards-container w-[100vw] md:w-full -mx-4 md:mx-0 mt-6 md:mt-[29px] xl:mt-8">
+                <div 
+                    className="flex flex-row w-full gap-4 md:gap-[21px] xl:gap-[27px] overflow-x-auto snap-x snap-mandatory touch-pan-x"
+                >
+                    {/* TODO */}
+                </div>
+            </div>
+        </div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,12 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Layout from '../components/layout'
+import PreviousRulings from '../components/PreviousRulings'
 
 const Home = () => {
   return (
     <Layout>
-        <h1 className='text-xl font-bold'>Por fin!</h1>
+        <PreviousRulings />
     </Layout>
   )
 }
